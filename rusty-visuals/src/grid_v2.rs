3314c416efe6e029e9eaabc1ef837eb79d9e0d74
Arjun Puri <arjunpur2@gmail.com>
2021-10-31 22:44:36 -0700

    Clean up code and add some tests

--- rusty-visuals/src/grid_v2.rs
+++ rusty-visuals/src/grid_v2.rs
@@ -13,20 +13,26 @@ use nannou::prelude::*;
 
 #[derive(Debug, Default, Clone)]
 pub struct Cell {
-    xy: Vector2,
-    wh: Vector2,
+    pub xy: Vector2,
+    pub wh: Vector2,
 
     // index is the (i,j) row, column index that this cell
     // represents.
-    index: Vector2<usize>,
+    pub index: Vector2<usize>,
 }
 
+/// Cell implements functions for a Grid container struct
+/// that represents a single block on the Grid.
 impl Cell {
+    // Width / Height; Coordinates; Index in 2D matix
     fn new(wh: Vector2, xy: Vector2, index: Vector2<usize>) -> Self {
         Cell { wh, xy, index }
     }
 }
 
+// A Grid is a collection of Cells in a 2D matrix of
+// arbitrary and possibly even non-uniform vectors
+// of vectors of cells.
 pub struct Grid {
     cells: Vec<Vec<Cell>>,
 }
@@ -43,8 +49,10 @@ impl Grid {
                 (0..num_cells.x)
                     .map(|col| {
                         Cell::new(
-                            vec2(cell_width, cell_height),
+                            vec2(cell_width, cell_height), // Dimensions
                             vec2(
+                                // Anchor from the top left of the grid and
+                                // build downwards and to the right.
                                 bounding_rect.top_left().x
                                     + ((cell_width / 2.0) * (col + 1) as f32),
                                 bounding_rect.top_left().y
@@ -55,12 +63,21 @@ impl Grid {
                     })
                     .collect()
             })
-            .collect();
+            .collect(); // Collect on both levels of vector
         Grid { cells }
     }
 
+    /// The row major iterator will traverse from the
+    /// top left of the matrix down to the bottom right
+    /// going cell by cell in each row.
     fn row_major_iter(&self) -> GridIterator {
         GridIterator::new(&self)
+    }
+
+    fn get_cell_by_index(&self, x: usize, y: usize) -> Option<&Cell> {
+        let row = self.cells.get(y);
+        let cell = row.map(|r| r.get(x)).flatten();
+        cell
     }
 }
 
@@ -86,6 +103,9 @@ impl<'a> Iterator for GridIterator<'a> {
         if row_idx >= self.grid.cells.len() {
             return None;
         }
+
+        // Move through the grid till you've found the next valid
+        // cell (i.e skip empty vectors in the middle)
         let mut row = self.grid.cells.get(row_idx).unwrap();
         while col_idx >= row.len() {
             row_idx += 1;
@@ -111,12 +131,13 @@ impl<'a> Iterator for GridIterator<'a> {
 mod tests {
     use super::Grid;
     use nannou::prelude::*;
+
     #[test]
-    fn can_construct_a_grid() {
+    fn indicies_are_correct() {
         let grid_cells = vec2(5, 5);
         let rect = geom::Rect::from_x_y_w_h(0.0, 0.0, 5.0, 5.0);
         let grid = Grid::new(&rect, grid_cells);
-        println!("{:?}", grid.cells);
+
         let mut iter = grid.row_major_iter();
         for y in 0..grid_cells.y {
             for x in 0..grid_cells.x {
@@ -124,10 +145,19 @@ mod tests {
                 assert!(cell.is_some());
                 assert_eq!(cell.unwrap().index.x, x);
                 assert_eq!(cell.unwrap().index.y, y);
-                if cell.is_some() {
-                    println!("({}, {}): {:?}", y, x, cell.unwrap());
-                }
             }
         }
+    }
+
+    #[test]
+    fn coordinates_are_correct() {
+        let grid_cells = vec2(5, 5);
+        let rect = geom::Rect::from_x_y_w_h(0.0, 0.0, 5.0, 5.0);
+        let grid = Grid::new(&rect, grid_cells);
+
+        let cell_option = grid.get_cell_by_index(0, 2);
+        assert!(cell_option.is_some());
+        let cell = cell_option.unwrap();
+        assert_eq!(cell.xy.x, 10.0);
     }
 }
