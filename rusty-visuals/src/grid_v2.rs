f95437a5031213bd3231936620e1782fdf14b6b2
Arjun Puri <arjunpur2@gmail.com>
2021-11-11 14:50:04 -0800

    Add tests for grid_v2

--- rusty-visuals/src/grid_v2.rs
+++ rusty-visuals/src/grid_v2.rs
@@ -12,20 +12,25 @@
 use nannou::prelude::*;
 
 #[derive(Debug, Default, Clone)]
+
+pub struct CellIndex {
+    row: usize,
+    col: usize,
+}
 pub struct Cell {
     pub xy: Vector2,
     pub wh: Vector2,
 
     // index is the (i,j) row, column index that this cell
     // represents.
-    pub index: Vector2<usize>,
+    pub index: CellIndex,
 }
 
 /// Cell implements functions for a Grid container struct
 /// that represents a single block on the Grid.
 impl Cell {
     // Width / Height; Coordinates; Index in 2D matix
-    fn new(wh: Vector2, xy: Vector2, index: Vector2<usize>) -> Self {
+    fn new(wh: Vector2, xy: Vector2, index: CellIndex) -> Self {
         Cell { wh, xy, index }
     }
 }
@@ -38,27 +43,34 @@ pub struct Grid {
 }
 
 impl Grid {
-    fn new(bounding_rect: &Rect, num_cells: Vector2<usize>) -> Self {
-        let cell_width = bounding_rect.w() / num_cells.x as f32;
-        let cell_height = bounding_rect.h() / num_cells.y as f32;
+    /// num_cells: (number of rows, number of columns)
+    /// NOTE: All the indexing and dimensions follow the matrix
+    /// indexing scheme (i.e (rows, columns)).
+    fn new(bounding_rect: &Rect, num_cells: &CellIndex) -> Self {
+        let cell_height = bounding_rect.h() / num_cells.row as f32;
+        let cell_width = bounding_rect.w() / num_cells.col as f32;
 
         // Construct the 2D grid of cells using the bounding rectangle passed into the
         // constructor as an anchor for dimensions and position.
-        let cells: Vec<Vec<Cell>> = (0..num_cells.y)
+        let cells: Vec<Vec<Cell>> = (0..num_cells.row)
             .map(|row| {
-                (0..num_cells.x)
+                (0..num_cells.col)
                     .map(|col| {
                         Cell::new(
                             vec2(cell_width, cell_height), // Dimensions
                             vec2(
                                 // Anchor from the top left of the grid and
                                 // build downwards and to the right.
-                                bounding_rect.top_left().x
-                                    + ((cell_width / 2.0) * (col + 1) as f32),
+                                // Note: Nannou grids decrease in x going left
+                                // but increase in y going up. Top left is
+                                // (-width, height)
+                                // The 0.5 is to center the coordinate in the middle
+                                // of the cell
+                                bounding_rect.top_left().x + (cell_width * (col as f32 + 0.5)),
                                 bounding_rect.top_left().y
-                                    + ((cell_height / 2.0) * (row + 1) as f32),
+                                    - (cell_height * (row as f32 + 0.5) as f32),
                             ),
-                            vec2(col, row),
+                            CellIndex { row, col },
                         )
                     })
                     .collect()
@@ -74,9 +86,9 @@ impl Grid {
         GridIterator::new(&self)
     }
 
-    fn get_cell_by_index(&self, x: usize, y: usize) -> Option<&Cell> {
-        let row = self.cells.get(y);
-        let cell = row.map(|r| r.get(x)).flatten();
+    fn get_cell_by_index(&self, row: usize, col: usize) -> Option<&Cell> {
+        let row = self.cells.get(row);
+        let cell = row.map(|r| r.get(col)).flatten();
         cell
     }
 }
@@ -123,41 +135,118 @@ impl<'a> Iterator for GridIterator<'a> {
     }
 }
 
-// TODO: Test the actual coordinates are correct
-// TODO: Test non-square grids
-// TODO: Test grids with some empty rows
-// TODO: Test grids where the rectangle starts at an offset
-// TODO: Test grids where the rectangle is not square
 mod tests {
-    use super::Grid;
+    use super::{CellIndex, Grid};
     use nannou::prelude::*;
 
     #[test]
-    fn indicies_are_correct() {
-        let grid_cells = vec2(5, 5);
+    fn all_indicies_are_correct() {
+        let grid_cells = CellIndex { row: 5, col: 5 };
         let rect = geom::Rect::from_x_y_w_h(0.0, 0.0, 5.0, 5.0);
-        let grid = Grid::new(&rect, grid_cells);
+        let grid = Grid::new(&rect, &grid_cells);
 
         let mut iter = grid.row_major_iter();
-        for y in 0..grid_cells.y {
-            for x in 0..grid_cells.x {
+        for y in 0..grid_cells.row {
+            for x in 0..grid_cells.col {
                 let cell = iter.next();
                 assert!(cell.is_some());
-                assert_eq!(cell.unwrap().index.x, x);
-                assert_eq!(cell.unwrap().index.y, y);
+                assert_eq!(cell.unwrap().index.row, y);
+                assert_eq!(cell.unwrap().index.col, x);
             }
         }
     }
 
     #[test]
-    fn coordinates_are_correct() {
-        let grid_cells = vec2(5, 5);
+    fn cell_has_correct_metadata() {
+        let grid_cells = CellIndex { row: 5, col: 5 };
         let rect = geom::Rect::from_x_y_w_h(0.0, 0.0, 5.0, 5.0);
-        let grid = Grid::new(&rect, grid_cells);
+        let grid = Grid::new(&rect, &grid_cells);
 
         let cell_option = grid.get_cell_by_index(0, 2);
-        assert!(cell_option.is_some());
+        assert!(cell_option.is_some(), "this cell should exist");
         let cell = cell_option.unwrap();
-        assert_eq!(cell.xy.x, 10.0);
+        assert_eq!(cell.index.row, 0);
+        assert_eq!(cell.index.col, 2);
+        assert_eq!(cell.wh.x, 1.0, "cell width and height should be 1.0");
+        assert_eq!(cell.wh.y, 1.0, "cell width and height should be 1.0");
+        assert_eq!(
+            cell.xy.y, 2.0,
+            "the top row should have a y value of height - cell_height / 2.0"
+        );
+        assert_eq!(
+            cell.xy.x, 0.0,
+            "the second column should have an x value of -width + (cell_height / 2.0) * col",
+        )
+    }
+
+    #[test]
+    fn grid_coordinates_are_correct_when_rect_is_offset() {
+        let grid_cells = CellIndex { row: 5, col: 5 };
+        let rect = geom::Rect::from_x_y_w_h(10.0, 10.0, 10.0, 10.0);
+        let grid = Grid::new(&rect, &grid_cells);
+
+        let cell_option = grid.get_cell_by_index(0, 0);
+        assert!(cell_option.is_some(), "this cell should exist");
+        let cell = cell_option.unwrap();
+        assert_eq!(cell.wh.x, 2.0);
+        assert_eq!(cell.wh.y, 2.0);
+        assert_eq!(cell.index.row, 0);
+        assert_eq!(cell.index.col, 0);
+        assert_eq!(cell.xy.x, 6.0);
+        assert_eq!(cell.xy.y, 14.0);
+
+        let cell_option = grid.get_cell_by_index(5, 5);
+        assert!(cell_option.is_none(), "this cell should NOT exist");
+
+        let cell_option = grid.get_cell_by_index(4, 4);
+        assert!(cell_option.is_some(), "this cell should exist");
+        let cell = cell_option.unwrap();
+        assert_eq!(cell.wh.x, 2.0);
+        assert_eq!(cell.wh.y, 2.0);
+        assert_eq!(cell.index.row, 4);
+        assert_eq!(cell.index.col, 4);
+        assert_eq!(cell.xy.x, 14.0);
+        assert_eq!(cell.xy.y, 6.0);
+    }
+
+    #[test]
+    fn grid_coordinates_are_correct_when_rect_is_not_square() {
+        let grid_cells = CellIndex { row: 10, col: 5 };
+        let rect = geom::Rect::from_x_y_w_h(0.0, 0.0, 10.0, 100.0);
+        let grid = Grid::new(&rect, &grid_cells);
+
+        let cell_option = grid.get_cell_by_index(0, 0);
+        assert!(cell_option.is_some(), "this cell should exist");
+        let cell = cell_option.unwrap();
+        assert_eq!(cell.wh.x, 2.0);
+        assert_eq!(cell.wh.y, 10.0);
+        assert_eq!(cell.index.row, 0);
+        assert_eq!(cell.index.col, 0);
+        assert_eq!(cell.xy.x, -4.0);
+        assert_eq!(cell.xy.y, 45.0);
+
+        let cell_option = grid.get_cell_by_index(0, 4);
+        assert!(cell_option.is_some(), "this cell should exist");
+        let cell = cell_option.unwrap();
+        assert_eq!(cell.index.row, 0);
+        assert_eq!(cell.index.col, 4);
+        assert_eq!(cell.xy.x, 4.0);
+        assert_eq!(cell.xy.y, 45.0);
+
+        let cell_option = grid.get_cell_by_index(9, 0);
+        assert!(cell_option.is_some(), "this cell should exist");
+        let cell = cell_option.unwrap();
+        assert_eq!(cell.index.row, 9);
+        assert_eq!(cell.index.col, 0);
+        assert_eq!(cell.xy.x, -4.0);
+        assert_eq!(cell.xy.y, -45.0);
+
+        let cell_option = grid.get_cell_by_index(9, 4);
+        assert!(cell_option.is_some(), "this cell should exist");
+        let cell = cell_option.unwrap();
+        assert_eq!(cell.index.row, 9);
+        assert_eq!(cell.index.col, 4);
+        assert_eq!(cell.xy.x, 4.0);
+        assert_eq!(cell.xy.y, -45.0);
     }
 }
